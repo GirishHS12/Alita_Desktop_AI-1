8070fa224ae03b7723d1a450e26ed76ef0672433
GirishHS <girishhsofficial@gmail.com>
2024-04-28 09:33:15 +0530

    porcupine was created successfully

--- engine/features.py
+++ engine/features.py
@@ -26,35 +26,36 @@ def play_youtube(query):
     pyautogui.hotkey('f')
 
 def hotword():
-    porcupine=None
-    paud=None
-    audio_stream=None
-    
+    porcupine = None
+    paud = None
+    audio_stream = None
     try:
-        #pre Trained keywords
-        porcupine=pvporcupine.create(access_key='${6R+qJMqUy7Pfv8aw5oPkMMroE4WTExVGQRnkUGWpL1ByXQ5BCgxpXw==}',keyword_paths=['${www\\assets\\alita keyword\\alita_en_windows_v3_0_0.ppn}'])
-        paud=pyaudio.PyAudio()
-        audio_stream=paud.open(rate=porcupine.sample_rate,channels=1,format=pyaudio.paInt16,input=True,frames_per_buffer=porcupine.frame_length)
-        
-        #loop for streaming
+
+        # pre trained keywords
+        porcupine = pvporcupine.create(access_key='6R+qJMqUy7Pfv8aw5oPkMMroE4WTExVGQRnkUGWpL1ByXQ5BCgxpXw==',keyword_paths=['C:\\Users\Girish\OneDrive\Desktop\Alita-AI\Alita AI\www\\assets\\alita keyword\\alita_en_windows_v3_0_0.ppn'])
+        paud = pyaudio.PyAudio()
+        audio_stream = paud.open(rate=porcupine.sample_rate, channels=1, format=pyaudio.paInt16, input=True,
+                                 frames_per_buffer=porcupine.frame_length)
+
+        # loop for streaming
         while True:
-            keyword=audio_stream.read(porcupine.frame_length)
-            keyword=struct.unpack_from("h"*porcupine.frame_length,keyword)
-            
-            #processing keyword comes mic
-            keyword_index=porcupine.process(keyword)
-            
-            #checking first keyword detected for not
-            if keyword_index>=0:
+            keyword = audio_stream.read(porcupine.frame_length)
+            keyword = struct.unpack_from("h" * porcupine.frame_length, keyword)
+
+            # processing keyword comes from mic
+            keyword_index = porcupine.process(keyword)
+
+            # checking first keyword detetcted for not
+            if keyword_index >= 0:
                 print("hotword detected")
-                
-                #pressing shortcut key win+j
+
+                # pressing shorcut key win+j
                 import pyautogui as autogui
                 autogui.keyDown("win")
                 autogui.press("j")
                 time.sleep(2)
                 autogui.keyUp("win")
-                
+
     except:
         if porcupine is not None:
             porcupine.delete()
@@ -62,9 +63,7 @@ def hotword():
             audio_stream.close()
         if paud is not None:
             paud.terminate()
-            
-    
-            
+
 
 def openCommand(query):
     query.lower()
--- test.py
+++ test.py
@@ -1,50 +1,48 @@
 import time
-import pyautogui
+# import pyautogui
 import pyaudio
 import pvporcupine
 import struct
 import os
 
-keyword_path='www\\assets\\alita keyword\\alita_en_windows_v3_0_0.ppn'
-porcupine = None
-keyword=['alita']
-
-try:
-    porcupine=pvporcupine.create(keyword_keywords=keyword,keyword_paths=[os.path.join(keyword_path,'www\\assets\\alita keyword\\alita_en_windows_v3_0_0.ppn')])
-except  pvporcupine.PorcupineException as e:
-    print(f"failed to create porcupine:{e}")
+# porcupine := Porcupine{
+#   AccessKey: "6R+qJMqUy7Pfv8aw5oPkMMroE4WTExVGQRnkUGWpL1ByXQ5BCgxpXw=="
+#     KeywordPaths:[]string{"C:\Users\Girish\OneDrive\Desktop\Alita-AI\Alita AI\www\\assets\\alita keyword\\alita_en_windows_v3_0_0.ppn"}
+# }
+#
+# err:= porcupine.Init()
 
 def hotword():
-
-    porcupine=None
-    paud=None
-    audio_stream=None
-    
+    porcupine = None
+    paud = None
+    audio_stream = None
     try:
-        #pre Trained keywords
-        porcupine=pvporcupine.create(access_key='${6R+qJMqUy7Pfv8aw5oPkMMroE4WTExVGQRnkUGWpL1ByXQ5BCgxpXw==}',keyword=['${www\\assets\\alita keyword\\alita_en_windows_v3_0_0.ppn}'])
-        paud=pyaudio.PyAudio()
-        audio_stream=paud.open(rate=porcupine.sample_rate,channels=1,format=pyaudio.paInt16,input=True,frames_per_buffer=porcupine.frame_length)
-        
-        #loop for streaming
+
+        # pre trained keywords
+        porcupine = pvporcupine.create(access_key='6R+qJMqUy7Pfv8aw5oPkMMroE4WTExVGQRnkUGWpL1ByXQ5BCgxpXw==',keyword_paths=['C:\\Users\Girish\OneDrive\Desktop\Alita-AI\Alita AI\www\\assets\\alita keyword\\alita_en_windows_v3_0_0.ppn'])
+        paud = pyaudio.PyAudio()
+        audio_stream = paud.open(rate=porcupine.sample_rate, channels=1, format=pyaudio.paInt16, input=True,
+                                 frames_per_buffer=porcupine.frame_length)
+
+        # loop for streaming
         while True:
-            keyword=audio_stream.read(porcupine.frame_length)
-            keyword=struct.unpack_from("h"*porcupine.frame_length,keyword)
-            
-            #processing keyword comes mic
-            keyword_index=porcupine.process(keyword)
-            
-            #checking first keyword detected for not
-            if keyword_index>=0:
+            keyword = audio_stream.read(porcupine.frame_length)
+            keyword = struct.unpack_from("h" * porcupine.frame_length, keyword)
+
+            # processing keyword comes from mic
+            keyword_index = porcupine.process(keyword)
+
+            # checking first keyword detetcted for not
+            if keyword_index >= 0:
                 print("hotword detected")
-                
-                #pressing shortcut key win+j
+
+                # pressing shorcut key win+j
                 import pyautogui as autogui
                 autogui.keyDown("win")
                 autogui.press("j")
                 time.sleep(2)
                 autogui.keyUp("win")
-                
+
     except:
         if porcupine is not None:
             porcupine.delete()
